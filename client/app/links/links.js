72ca1019cb2c6340ff0227404a250c2b810f6178
Kate Jefferson <barbedbella@gmail.com>
2015-03-06 18:46:14 -0600

    Passed initial tests.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,15 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+    $scope.data = {};
+    $scope.getLinks = function () {
+      Links.getLinks($scope.data)
+        .then(function(data) {
+          $scope.data.links = data;
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    };
+    $scope.getLinks();
 });
